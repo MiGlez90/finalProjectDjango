ed1fdaa2849eba1123f24763f0519f55b28ddca9
Miguel Rafael González Durón <miglez90290995@gmail.com>
2017-12-10 19:03:49 -0600

    Creation of api

--- Profile/views.py
+++ Profile/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import ProfileSerializer
+from  .models import Profile
 # Create your views here.
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
