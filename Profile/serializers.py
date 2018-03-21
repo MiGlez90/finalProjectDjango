c98ca4287895ef7a1c9086f948797439a53f441f
Miguel Gonzalez <miglez90290995@gmail.com>
2018-03-20 20:12:10 -0600

    changes on Model and ViewSets :octocat:

--- Profile/serializers.py
+++ Profile/serializers.py
@@ -1,10 +1,61 @@
 from rest_framework import serializers
-from .models import Profile, College
+from .models import Profile, College, Tutor, Address
 from accounts.serializers import UserSerializer
 from django.contrib.auth.models import User
 
+class Base64ImageField(serializers.ImageField):
+    """
+    A Django REST framework field for handling image-uploads through raw post data.
+    It uses base64 for encoding and decoding the contents of the file.
+
+    Heavily based on
+    https://github.com/tomchristie/django-rest-framework/pull/1268
+
+    Updated for Django REST framework 3.
+    """
+
+    def to_internal_value(self, data):
+        from django.core.files.base import ContentFile
+        import base64
+        import six
+        import uuid
+
+        # Check if this is a base64 string
+        if isinstance(data, six.string_types):
+            # Check if the base64 string is in the "data:" format
+            if 'data:' in data and ';base64,' in data:
+                # Break out the header from the base64 content
+                header, data = data.split(';base64,')
+
+            # Try to decode the file. Return validation error if it fails.
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+
+            # Generate file name:
+            file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
+            # Get the file name extension:
+            file_extension = self.get_file_extension(file_name, decoded_file)
+
+            complete_file_name = "%s.%s" % (file_name, file_extension, )
+
+            data = ContentFile(decoded_file, name=complete_file_name)
+
+        return super(Base64ImageField, self).to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+        import imghdr
+
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
+
+        return extension
+
 
 class BasicProfileSerializer(serializers.ModelSerializer):
+    profilePicture = Base64ImageField(max_length=None,use_url=True)
+    wallPicture = Base64ImageField(max_length=None,use_url=True)
     class Meta:
         model = Profile
         fields = '__all__'
@@ -12,13 +63,17 @@ class BasicProfileSerializer(serializers.ModelSerializer):
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False,read_only=True)
-    # profilePicture = serializers.Field('image.url')
+    profilePicture = Base64ImageField(max_length=None, use_url=True)
+    wallPicture = Base64ImageField(max_length=None, use_url=True)
     class Meta:
         model = Profile
         fields = '__all__'
 
 
+
 class CollegeSerializer(serializers.ModelSerializer):
+    profilePicture = Base64ImageField(max_length=None, use_url=True)
+    wallPicture = Base64ImageField(max_length=None, use_url=True)
     class Meta:
         model = College
         fields = '__all__'
@@ -26,6 +81,37 @@ class CollegeSerializer(serializers.ModelSerializer):
 
 class UserWithProfileSerializer(serializers.ModelSerializer):
     profile = BasicProfileSerializer(many=False,read_only=True)
+    profile.profilePicture = Base64ImageField(max_length=None, use_url=True)
+    profile.wallPicture = Base64ImageField(max_length=None,use_url=True)
+
     class Meta:
         model = User
         fields = ['username', 'profile', 'id', 'email', 'is_staff', 'first_name', 'last_name']
+
+
+class BasicAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class BasicTutorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tutor
+        fields = '__all__'
+
+
+class TutorSerializer(serializers.ModelSerializer):
+    address = BasicAddressSerializer(many=False,read_only=True)
+
+    class Meta:
+        model = Tutor
+        fields = '__all__'
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    profile = BasicProfileSerializer(many=False,read_only=True)
+
+    class Meta:
+        model = Address
+        fields = '__all__'
--- Profile/views.py
+++ Profile/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import BasicProfileSerializer, ProfileSerializer, CollegeSerializer, UserWithProfileSerializer
-from .models import Profile, College
+from .serializers import BasicProfileSerializer, ProfileSerializer, CollegeSerializer, UserWithProfileSerializer, TutorSerializer, BasicTutorSerializer, AddressSerializer, BasicAddressSerializer
+from .models import Profile, College, Tutor, Address
 from django.contrib.auth.models import User
 from rest_framework import views
 from rest_framework.response import Response
@@ -35,4 +35,35 @@ class ProfileWithToken(viewsets.ModelViewSet):
 class UserView(views.APIView):
     def get(self, request):
         user = request.user
-        return Response(UserWithProfileSerializer(user).data)
+        return Response(UserWithProfileSerializer(user,context={"request": request}).data)
+
+
+class TutorViewSet(viewsets.ModelViewSet):
+    queryset = Tutor.objects.all()
+    serializer_class = TutorSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return TutorSerializer
+        if self.action == 'retrieve':
+            return TutorSerializer
+        return BasicTutorSerializer
+
+    def get_queryset(self):
+        # Recupera el usuario que hace la petición
+        user = self.request.user
+        # Crea el queryset
+        # Filtra y recupera solo los proyectos que pertenecen al usuario
+        return Tutor.objects.filter(user=user)
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return AddressSerializer
+        if self.action == 'retrieve':
+            return AddressSerializer
+        return BasicAddressSerializer
